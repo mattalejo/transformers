50010bea515bffc6a7c9a092a2fd4c55cef9435c
Matt A <72350251+mattalejo@users.noreply.github.com>
2023-10-13 08:09:43 +0800

    Update modeling_time_series_transformer.py

--- src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
+++ src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
@@ -1332,12 +1332,13 @@ class TimeSeriesTransformerModel(TimeSeriesTransformerPreTrainedModel):
         log_scale = scale.log() if self.config.input_size == 1 else scale.squeeze(1).log()
         static_feat = torch.cat((log_abs_loc, log_scale), dim=1)
 
-        if static_real_features is not None:
-            static_feat = torch.cat((static_real_features, static_feat), dim=1)
-        if static_categorical_features is not None:
-            embedded_cat = self.embedder(static_categorical_features)
-            static_feat = torch.cat((embedded_cat, static_feat), dim=1)
-        expanded_static_feat = static_feat.unsqueeze(1).expand(-1, time_feat.shape[1], -1)
+        # if static_real_features is not None:
+        #     static_feat = torch.cat((static_real_features, static_feat), dim=1)
+        # if static_categorical_features is not None:
+        #     embedded_cat = self.embedder(static_categorical_features)
+        #     static_feat = torch.cat((embedded_cat, static_feat), dim=1)
+        
+        expanded_static_feat = static_feat.expand(-1, time_feat.shape[1], -1)
 
         # all features
         features = torch.cat((expanded_static_feat, time_feat), dim=-1)
